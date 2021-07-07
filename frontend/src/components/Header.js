bf93cfc5cb13542c65fc7ecabd8685e472d2b8b4
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-07 13:21:21 +0500

    Added fontawsome and navbar theme

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,20 +5,22 @@ import { Navbar, Nav, Container, Row } from 'react-bootstrap';
 function Header() {
     return (
         <header>
-            <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-  <Navbar.Toggle aria-controls="navbarScroll" />
-  <Navbar.Collapse id="navbarScroll">
-    <Nav
-      className="mr-auto my-2 my-lg-0"
-      style={{ maxHeight: '100px' }}
-      navbarScroll
-    >
-      <Nav.Link href="#action1">Home</Nav.Link>
-      <Nav.Link href="#action2">Link</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+          <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+            <Container>
+              <Navbar.Brand href="/">cartya</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="mr-auto my-2 my-lg-0"
+                  style={{ maxHeight: '100px' }}
+                  navbarScroll
+                >
+                  <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                  <Nav.Link href="/login"><i className="fas fa-user"></i> Login</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
         </header>
     )
 }
