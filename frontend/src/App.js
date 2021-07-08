f513bdd1a14ae3a1b56bf90252ba8e9e3b62001c
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-08 12:06:12 +0500

    Adding react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
 import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Home />
+          <Route path='/' component={Home} exact />
+          <Route path='/product/:id' component={ProductDetail} />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/ProductDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Row, Col } from 'react-bootstrap';
+
+function ProductDetail() {
+    return (
+        <home>
+            Products
+        </home>
+    )
+}
+
+export default ProductDetail
