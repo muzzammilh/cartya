3a79f2fbaa13b75d7fc40e20915d2397d562ec3e
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-13 17:52:52 +0500

    Adding product model

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # image = 
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(max_digits=7, decimal_places=2)
+    num_reviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    count_in_stock = models.IntegerField(null=True, blank=True, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
