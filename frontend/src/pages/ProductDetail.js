2e8b9f5491f66d8de007b3a1ca05ca3be701572b
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-13 12:58:26 +0500

    Fetch data from API

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 import Rating from '../components/Rating';
-import products from '../products';
 
 function ProductDetail({ match }) {
-    const product = products.find( (p) => p._id == match.params.id );
+    
+    const [product, setProduct] = useState([]);
+
+    useEffect( () => {
+        
+        async function fetchProduct(){
+            const { data } = await axios.get(`/api/products/${match.params.id}`);
+            setProduct(data);
+        }
+
+        fetchProduct();
+
+
+    }, []);
+
     return (
         <div>
             <Link to="/" className="btn btn-dark my-3">Go Back</Link>
