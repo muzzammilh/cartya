2386fea939fd6f502d437f488629ad6e1d9f9fc1
Muzzammil Hussain <muzzammilz@gmail.com>
2021-07-19 14:48:02 +0500

    Added redux data into product detail page

--- frontend/src/pages/ProductDetail.js
+++ frontend/src/pages/ProductDetail.js
@@ -1,73 +1,76 @@
 import React, { useState, useEffect } from 'react';
 
+import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 import Rating from '../components/Rating';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import { listProductDetail } from '../actions/productActions';
 
 function ProductDetail({ match }) {
-    
-    const [product, setProduct] = useState([]);
+    const dispatch = useDispatch();
+    const productDetail = useSelector(state => state.productDetail)
+    const { error, loading, product } = productDetail
 
     useEffect( () => {
-        
-        async function fetchProduct(){
-            const { data } = await axios.get(`/api/products/${match.params.id}`);
-            setProduct(data);
-        }
-
-        fetchProduct();
-
-
-    }, []);
+        dispatch(listProductDetail(match.params.id));
+    }, [dispatch, match]);
 
     return (
         <div>
             <Link to="/" className="btn btn-dark my-3">Go Back</Link>
-            <Row>
-                <Col md={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col md={3}>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Rating value={product.rating} text={`${product.num_reviews} reviews`} color={'#f8e825'} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            Description: {product.description}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col md={3}>
-                    <Card>
-                        <ListGroup variant="flush" className="p-1">
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Price: </Col>
-                                    <Col><strong>${product.price}</strong></Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Col>Status: </Col>
-                                    <Col>
-                                        {product.count_in_stock > 0 ? 'In Stock' : 'Out of Stock'}
-                                    </Col>
-                                </Row>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Row>
-                                    <Button variant="primary" disabled={product.count_in_stock == 0}>Add to Cart</Button>
-                                </Row>
-                            </ListGroup.Item>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
+
+            { 
+                loading ? <Loader /> : 
+                error ? <Message variant='danger'> { error } </Message> :
+                (
+                    <Row>
+                        <Col md={6}>
+                            <Image src={product.image} alt={product.name} fluid />
+                        </Col>
+                        <Col md={3}>
+                            <ListGroup variant="flush">
+                                <ListGroup.Item>
+                                    <h3>{product.name}</h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Rating value={product.rating} text={`${product.num_reviews} reviews`} color={'#f8e825'} />
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    Description: {product.description}
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col md={3}>
+                            <Card>
+                                <ListGroup variant="flush" className="p-1">
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Price: </Col>
+                                            <Col><strong>${product.price}</strong></Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Col>Status: </Col>
+                                            <Col>
+                                                {product.count_in_stock > 0 ? 'In Stock' : 'Out of Stock'}
+                                            </Col>
+                                        </Row>
+                                    </ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <Row>
+                                            <Button variant="primary" disabled={product.count_in_stock == 0}>Add to Cart</Button>
+                                        </Row>
+                                    </ListGroup.Item>
+                                </ListGroup>
+                            </Card>
+                        </Col>
+                    </Row>
+                )
+            }
         </div>
     )
 }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,11 @@
 import { 
     PRODUCT_LIST_REQUEST, 
     PRODUCT_LIST_SUCCESS, 
-    PRODUCT_LIST_FAIL 
+    PRODUCT_LIST_FAIL ,
+
+    PRODUCT_DETAIL_REQUEST, 
+    PRODUCT_DETAIL_SUCCESS, 
+    PRODUCT_DETAIL_FAIL 
 } from '../constants/productConstants';
 
 export const productListReducer = (state = {products: []}, action) => {
@@ -14,6 +18,24 @@ export const productListReducer = (state = {products: []}, action) => {
             return {loading: false, products: action.payload}
 
         case PRODUCT_LIST_FAIL:
+            return {loading: false, error: action.payload}            
+        
+        default:
+            return state
+    }
+
+} 
+
+export const productDetailReducer = (state = {product: {reviews: []}}, action) => {
+
+    switch (action.type){
+        case PRODUCT_DETAIL_REQUEST:
+            return {loading: true, ...state}
+        
+        case PRODUCT_DETAIL_SUCCESS:
+            return {loading: false, product: action.payload}
+
+        case PRODUCT_DETAIL_FAIL:
             return {loading: false, error: action.payload}            
         
         default:
